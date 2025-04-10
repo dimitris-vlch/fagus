a10e6deb8d325dcffa500b7bb25ea10b50e00492
Dimitrios Vlachos <dimitris.vlachos.mbg@gmail.com>
2025-04-10 07:18:15 +0300

    graphs

--- visualization.py
+++ visualization.py
@@ -1,6 +1,7 @@
 # Σκριπτάκι για την γραφική αναπαράσταση των αποτελεσμάτων σε Bar και Pie chart
 
-# Βήμα 1: Εισαγωγή βιλβιοθηκών και ανάγνωση του αρχείου json, μετατροπή του σε λεξικό python.
+# Βήμα 1: Εισαγωγή βιλβιοθηκών και ανάγνωση του αρχείου json, μετατροπή του σε λεξικό python και υπολογισμός του συνολικού αριθμού των δειγμάτων (total_samples).
+
 # Αξιοποιούμε το υποπακέτο pyplot της βιβλιοθήκης matplotlib. Το plt ειναι alias για matplotlib.pyplot. Το pyplot έχει συναρτήσεις για γραφικά διαγράμματα, bar chart, pie charts, συναρτήσεις για τίτλους και ετικέτες, καθώς και για να εμφανίσεις τα διαγράμματα.
 # Εισάγουμε την υποβιβλιοθήκη Counter από την βιβλιοθήκη collections. Το Counter το αξιοποιούμε για να δούμε πόσες φορές εμφανίζεται κάθε τιμή. Μας είναι χρήσιμο επειδή έχουμε πολλά δείγματα και κάθε δείγμα έχει διαφορετικά μεταδεδομένα γεωγραφίας, βλέπουμε απλώς την συχνότητα εμφάνισης της τοποθεσίας, της χώρας κλπ.
 
@@ -10,6 +11,8 @@ from collections import Counter
 
 with open("results_merged_json.txt", "r", encoding="utf-8") as file:
     data = json.load(file)
+
+total_samples = len(data)
 
 # Βήμα 2: Ορίζουμε ένα λεξικό, geo_fields, οπου κάθε αντικείμενό του (fields) είναι ένα πεδίο των αντικειμένων του λεξικού data, το οποίο και όμως φέρει γεωγραφική πληροφορία. Πεδιά του στύλ χώρα, συντεταγμένες lat, lon, κλπ.
 
@@ -53,6 +56,12 @@ for registry in data:
     else:
         without_geo += 1
 
+# Βήμα 5: Εκτύπωση ορισμένων βασικών αποτελεσμάτων: Εκτυπώνονται οι τιμές της κάθε μεταβλητής που αντιπροσωπεύει μέγεθος ενδιαφέροντος και στην συνέχεια το ποσοστό αυτού ως προ τοο σύνολο των δειγμάτων με ένα δεκαδικό ψηφίο.
+
+print(f"Total number of samples: {total_samples}, {total_samples / total_samples:.1%}")
+print(f"Total number of samples with geological information: {with_geo} ({with_geo / total_samples:.1%})")
+print(f"Samples without total abscence of geological information: {without_geo} ({without_geo / total_samples}) ")
+
 # Βήμα 5: Εκτύπωση των αποτελεσμάτων της καταμέτρησης
 # for geo_field, count in frequency.items(): Eκτελούμε βρόγχο loop στο αντικείμενο frequency, το οποίο και είναι τύπου Counter.
 # .items() Σε λεξικά και σε μεταβλητές τύπου Counter, επιστρέφει σε ζευγάρια (key,value). Έτσι, αφου ολοκληρώθηκε το βήμα 4, θα ισχύει frequency = Counter({ "country": 4435, "lat": 853, "lon": 853, "location": 200 }).
@@ -62,7 +71,7 @@ for registry in data:
 # εκτυπώνουμε στην συνέχεια κάθε πεδίο geo_field, συνοδευόμενο από τον ακέραιο αριθμό του count που μετρήσαμε με τη             frequency[geo_field] += 1 
 
 for geo_field, count in frequency.items():
-    print(f"{geo_field}: {count}")
+    print(f"Samples with {geo_field}: {count}")
 
 # Βήμα 6: Pie chart
 # Καταρχήν ορίζουμε labels, sizes, colors.
