f006ffa2475df2fb02dccbab97cf1ebb24a83896
Dimitrios Vlachos <dimitris.vlachos.mbg@gmail.com>
2025-04-24 22:30:57 +0300

    graphs

--- test.py
+++ test.py
@@ -23,6 +23,16 @@ geopandas_geo_dataframe = geopandas_naturalearth_lowres.explode(index_parts=Fals
 combined_geo_dataframe = gpd.sjoin(geo_dataframe, geopandas_geo_dataframe, how = "left", predicate = "within")
 
 
+with open("curated_data_json.txt", "r", encoding="utf-8")as file:
+    curated_data = json.load(file)
+
+# Οπτικοποίηση των αποτελεσμάτων:
+
+# Γιατί ήταν σημαντικές οι παραπάνω διορθώσεις;
+
+# Διάγραμμα 1: Bar Plot που συγκρίνει τον αριθμό των αποτελεσμάτων mismatch ύστερα από την επεξεργασία των συντεταγμένων με τον geopandas χάρτη πολυγώνων naturalearth_lowres, τον αριθμό των mismatch μετά την διόρθωση για Σερβία, τον αριθμό των mismatch μετά την διόρθωση για ΗΠΑ και τον αριθμό των mismatch μετά την συμπλήρωση χώρας με Nominatim για τις συντεταγμένες που δεν εντοπίζονται στο χάρτη naturalearth_lowres.
+
+# Yπολογίζω mismatches αποκλειστικά με geopandas, δεν έχουμε άκομα ανιχνεύσει καμία ψευδώς θετική αναντιστοιχία χώρας-συντεταγμένων:
 
 calculated_mismatches_1 = 0
 false_positive_mismatches = 0 
@@ -35,6 +45,7 @@ for _, row in combined_geo_dataframe.iterrows():
 print(f"\nΥπολογίζοντας αποκλειστικά με geopandas, βρίσκω {calculated_mismatches_1} αναντιστοιχίες χώρας-συντεταγμένων και ανιχνεύω {false_positive_mismatches} ψευδώς θετικές αναντιστοιχίες")
 
 calculated_mismatches_2 = 0
+false_positive_mismatches = 0 
 
 # Υπολογίζω mismatches με geopandas και πραγματοποιώ την διόρθωση για ΗΠΑ
 
@@ -52,6 +63,45 @@ for _, row in combined_geo_dataframe.iterrows():
 
 false_positive_mismatches = calculated_mismatches_1 - calculated_mismatches_2
 
-print(f"\nΥπολογίζοντας αποκλειστικά με geopandas, βρίσκω {calculated_mismatches_2} αναντιστοιχίες χώρας-συντεταγμένων και ανιχνεύω {false_positive_mismatches} ψευδώς θετικές αναντιστοιχίες")
+print(f"\nΥπολογίζοντας με geopandas και διορθώνοντας για ΗΠΑ, βρίσκω {calculated_mismatches_2} αναντιστοιχίες χώρας-συντεταγμένων και ανιχνεύω {false_positive_mismatches} ψευδώς θετικές αναντιστοιχίες")
 
+# Υπολογίζω mismatches με geopandas και πραγματοποιώ την διόρθωση για ΗΠΑ και Σερβία
+
+calculated_mismatches_3 = 0
+false_positive_mismatches = 0 
+
+
+for _, row in combined_geo_dataframe.iterrows():
     
+    suggested = row["ADMIN"]
+    
+    if suggested == "United States of America":
+        suggested = "USA"
+    elif suggested == "Republic of Serbia":
+        suggested = "Serbia"
+        country_match = "yes" if row["country_submitted"] == suggested else "no"
+
+    country_match = "yes" if row["country_submitted"] == row["ADMIN"] or row["country_submitted"] == suggested else "no"
+    
+    if country_match == "no":
+        calculated_mismatches_3 += 1
+
+false_positive_mismatches = calculated_mismatches_1 - calculated_mismatches_3
+
+print(f"\nΥπολογίζοντας με geopandas, και διορθώνοντας για ΗΠΑ και Σερβία, βρίσκω {calculated_mismatches_3} αναντιστοιχίες χώρας-συντεταγμένων και ανιχνεύω {false_positive_mismatches} ψευδώς θετικές αναντιστοιχίες")
+
+false_positive_mismatches = 0 
+
+match = 0
+mismatch = 0
+
+for registry in curated_data:
+    if registry["country_match"] == "yes":
+        match +=1
+    else:
+        mismatch +=1
+
+
+false_positive_mismatches = calculated_mismatches_1 - mismatch
+
+print(f"\nΥπολογίζοντας με geopandas και  και διορθώνοντας για ΗΠΑ και Σερβία, βρίσκω {mismatch} αναντιστοιχίες χώρας-συντεταγμένων και ανιχνεύω {false_positive_mismatches} ψευδώς θετικές αναντιστοιχίες")
