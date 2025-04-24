53372224a1044feee93fe6c3ee7501462bdf7893
Dimitrios Vlachos <dimitris.vlachos.mbg@gmail.com>
2025-04-25 01:49:43 +0300

    graphs

--- mismatches_and_reverse_geocoding.py
+++ mismatches_and_reverse_geocoding.py
@@ -143,20 +143,20 @@ print(f"\ncurated_data_json.txt has been created successfully! This json file is
 
 # Yπολογίζω mismatches αποκλειστικά με geopandas, δεν έχουμε άκομα ανιχνεύσει καμία ψευδώς θετική αναντιστοιχία χώρας-συντεταγμένων:
 
-calculated_mismatches = 0
+calculated_mismatches_1 = 0
 false_positive_mismatches = 0 
 
 for _, row in combined_geo_dataframe.iterrows():
     country_match = "yes" if row["country_submitted"] == row["ADMIN"] else "no"
     if country_match == "no":
-        calculated_mismatches += 1
+        calculated_mismatches_1 += 1
 
-print(f"\nΥπολογίζοντας αποκλειστικά με geopandas, βρίσκω {calculated_mismatches} αναντιστοιχίες χώρας-συντεταγμένων και ανιχνεύω {false_positive_mismatches} ψευδώς θετικές αναντιστοιχίες")
+print(f"\nΥπολογίζοντας αποκλειστικά με geopandas, βρίσκω {calculated_mismatches_1} αναντιστοιχίες χώρας-συντεταγμένων και ανιχνεύω {false_positive_mismatches} ψευδώς θετικές αναντιστοιχίες")
 
-calculated_mismatches = 0
+calculated_mismatches_2 = 0
 false_positive_mismatches = 0 
 
-# Υπολογίζω mismatches με geopandas και πραγματοποιώ την διόρθωση για ΗΠΑ
+# Υπολογίζω mismatches με geopandas και πραγματοποιώ την διόρθωση για ΗΠΑ.
 
 for _, row in combined_geo_dataframe.iterrows():
     
@@ -168,8 +168,52 @@ for _, row in combined_geo_dataframe.iterrows():
     country_match = "yes" if row["country_submitted"] == row["ADMIN"] or row["country_submitted"] == suggested else "no"
     
     if country_match == "no":
-        calculated_mismatches += 1
+        calculated_mismatches_2 += 1
 
-print(f"\nΥπολογίζοντας αποκλειστικά με geopandas, βρίσκω {calculated_mismatches} αναντιστοιχίες χώρας-συντεταγμένων και ανιχνεύω {false_positive_mismatches} ψευδώς θετικές αναντιστοιχίες")
+false_positive_mismatches = calculated_mismatches_1 - calculated_mismatches_2
 
+print(f"\nΥπολογίζοντας με geopandas και διορθώνοντας για ΗΠΑ, βρίσκω {calculated_mismatches_2} αναντιστοιχίες χώρας-συντεταγμένων και ανιχνεύω {false_positive_mismatches} ψευδώς θετικές αναντιστοιχίες")
+
+# Υπολογίζω mismatches με geopandas και πραγματοποιώ την διόρθωση για ΗΠΑ και Σερβία.
+
+calculated_mismatches_3 = 0
+false_positive_mismatches = 0 
+
+
+for _, row in combined_geo_dataframe.iterrows():
     
+    suggested = row["ADMIN"]
+    
+    if suggested == "United States of America":
+        suggested = "USA"
+    elif suggested == "Republic of Serbia":
+        suggested = "Serbia"
+        country_match = "yes" if row["country_submitted"] == suggested else "no"
+
+    country_match = "yes" if row["country_submitted"] == row["ADMIN"] or row["country_submitted"] == suggested else "no"
+    
+    if country_match == "no":
+        calculated_mismatches_3 += 1
+
+false_positive_mismatches = calculated_mismatches_1 - calculated_mismatches_3
+
+print(f"\nΥπολογίζοντας με geopandas, και διορθώνοντας για ΗΠΑ και Σερβία, βρίσκω {calculated_mismatches_3} αναντιστοιχίες χώρας-συντεταγμένων και ανιχνεύω {false_positive_mismatches} ψευδώς θετικές αναντιστοιχίες")
+
+# Υπολογίζω mismatches με geopandas και Nominatim και πραγματοποιώ την διόρθωση για ΗΠΑ και Σερβία.
+
+
+false_positive_mismatches = 0 
+
+match = 0
+mismatch = 0
+
+for registry in curated_data:
+    if registry["country_match"] == "yes":
+        match +=1
+    else:
+        mismatch +=1
+
+
+false_positive_mismatches = calculated_mismatches_1 - mismatch
+
+print(f"\nΥπολογίζοντας με geopandas και Nominatim και διορθώνοντας για ΗΠΑ και Σερβία, βρίσκω {mismatch} αναντιστοιχίες χώρας-συντεταγμένων και ανιχνεύω {false_positive_mismatches} ψευδώς θετικές αναντιστοιχίες")
